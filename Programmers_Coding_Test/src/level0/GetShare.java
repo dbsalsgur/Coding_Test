aac4016ee302862e5c4ab953a1ba4a6dfb4936fc
dbsalsgur <rkawkaos123@naver.com>
2023-03-20 20:48:45 +0900

    230320 coding test 연습

---
+++ Programmers_Coding_Test/src/level0/GetShare.java
@@ -0,0 +1,11 @@
+package level0;
+
+public class GetShare {
+
+	 public int solution(int num1, int num2) {
+	        int answer = 0;
+	        answer = num1/num2;
+	        return answer;
+	    }
+	 
+}
