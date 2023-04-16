e5deff17520db36e61b9da7c4995d3b2cb41a7b4
dbsalsgur <rkawkaos123@naver.com>
2023-04-16 22:53:24 +0900

    230416 coding test 연습

---
+++ Programmers_Coding_Test/src/level0/ReverseArray.java
@@ -0,0 +1,16 @@
+package level0;
+
+public class ReverseArray {
+
+	public int[] solution(int[] num_list) {
+        int[] answer = {};
+        return answer;
+    }
+	
+}
+//문제 설명
+//정수가 들어 있는 배열 num_list가 매개변수로 주어집니다. num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 solution 함수를 완성해주세요.
+//
+//제한사항
+//1 ≤ num_list의 길이 ≤ 1,000
+//0 ≤ num_list의 원소 ≤ 1,000
