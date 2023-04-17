417f0953d72ac26e05bbd755d78c12bb8c879a4c
dbsalsgur <rkawkaos123@naver.com>
2023-04-17 23:56:23 +0900

    230417 coding test 연습

--- Programmers_Coding_Test/src/level0/ReverseArray.java
+++ Programmers_Coding_Test/src/level0/ReverseArray.java
@@ -3,7 +3,10 @@ package level0;
 public class ReverseArray {
 
 	public int[] solution(int[] num_list) {
-        int[] answer = {};
+        int[] answer = new int[num_list.length];
+        for (int i = 0; i < num_list.length; i++) {
+			answer[i] = num_list[num_list.length-1-i];
+		}
         return answer;
     }
 	
