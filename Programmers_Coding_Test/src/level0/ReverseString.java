be184d14e7973b8c7db31981a094b3a32b869694
dbsalsgur <rkawkaos123@naver.com>
2023-04-21 23:56:19 +0900

    230421 coding test 연습

--- Programmers_Coding_Test/src/level0/ReverseString.java
+++ Programmers_Coding_Test/src/level0/ReverseString.java
@@ -3,7 +3,11 @@ package level0;
 public class ReverseString {
 
 	public String solution(String my_string) {
-        String answer = "";
+		char[] str = my_string.toCharArray();
+		String answer = "";
+		for (int i = 0; i < str.length; i++) {
+			answer += str[str.length-1-i];
+		}
         return answer;
     }
 	
--- Programmers_Coding_Test/src/level0/Test.java
+++ Programmers_Coding_Test/src/level0/Test.java
@@ -3,21 +3,13 @@ package level0;
 public class Test {
 
 	public static void main(String[] args) {
-		int n = 10;
-		int temp = 0;
-		int answer = 0;
-		if (n%6 == 0) {
-			answer = n/6;
-		} else {
-			for (int i = 1; i <= n*6; i++) {
-				if (i%n == 0 && i%6 == 0) {
-					temp = i;
-					break;
-				}
-			}
+		String my_string = "rkawkaos";
+		char[] str = my_string.toCharArray();
+		String answer = "";
+		for (int i = 0; i < str.length; i++) {
+			answer += str[str.length-1-i];
 		}
-		answer = temp/6;
-		System.out.println(answer);
+        System.out.println(answer);
 	}
 }
 
