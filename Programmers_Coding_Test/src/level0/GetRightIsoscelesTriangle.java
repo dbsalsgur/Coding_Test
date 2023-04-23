e693df411f4e4adfd9d13f5de0c643cb2ea13007
dbsalsgur <rkawkaos123@naver.com>
2023-04-23 21:19:00 +0900

    230423 coding test 연습

---
+++ Programmers_Coding_Test/src/level0/GetRightIsoscelesTriangle.java
@@ -0,0 +1,13 @@
+package level0;
+
+import java.util.Scanner;
+
+public class GetRightIsoscelesTriangle {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(n);
+    }
+}
