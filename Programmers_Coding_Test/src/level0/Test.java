9c4f0b1b1084699e0c5cbe3f90eacedc11d7c5b2
dbsalsgur <rkawkaos123@naver.com>
2023-04-29 19:15:16 +0900

    230429 coding test 연습

--- Programmers_Coding_Test/src/level0/Test.java
+++ Programmers_Coding_Test/src/level0/Test.java
@@ -1,19 +1,10 @@
 package level0;
 
-import java.util.Scanner;
 
 public class Test {
 
 	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        for (int i = 1; i <= n; i++) {
-        	for (int j = 1; j <= i; j++) {
-				System.out.print("*");
-			}
-        	System.out.println();
-		}
+		int[] answer = new int[2];
     }
 }
 
