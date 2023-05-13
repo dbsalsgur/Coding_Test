b2c4e0a4361def270b8137c6b68a4cb84007becd
dbsalsgur <rkawkaos123@naver.com>
2023-05-13 22:30:21 +0900

    230513 coding test 연습

--- Programmers_Coding_Test/src/level0/Test.java
+++ Programmers_Coding_Test/src/level0/Test.java
@@ -1,47 +1,17 @@
 package level0;
 
-import java.util.HashMap;
-
 public class Test {
 
 	public static void main(String[] args) {
-		
-		int[] emergency = {30, 10, 23, 6, 100};
-		int[] sortEmergency = emergency.clone();
-		
-        int[] answer = new int[emergency.length];
-        sort(sortEmergency);
-        for (int i : sortEmergency) {
-			System.out.println(i);
-		}
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 1; i <= answer.length; i++) {
-			map.put(sortEmergency[answer.length-i], i);
-		}
-        for (int i = 0; i < answer.length; i++) {
-			answer[i] = map.get(emergency[i]);
-		}
-        for (int i : answer) {
-			System.out.println(i);
-		}
-    }
-	
-	public static void sort(int[] array) {
-		int temp = 0;
-		for (int i = 0; i < array.length-1; i++) {
-			for (int j = 0; j < array.length-1; j++) {
-				if (array[j] > array[j+1]) {
-					temp = array[j];
-					array[j] = array[j+1];
-					array[j+1] = temp;
-				}
-				for (int k : array) {
-					System.out.print(k+", ");
-				}
-				System.out.println();
+		int n = 20;
+		int answer = 0;
+		for (int i = 1; i <= n; i++) {
+			if (n%i == 0) {
+				answer++;
 			}
 		}
-
+		System.out.println(answer);
+		
 	}
 }
 
