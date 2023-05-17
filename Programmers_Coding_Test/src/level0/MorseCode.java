590d46ba5c59edb16581a9fdd5ba9a37f54ef790
dbsalsgur <rkawkaos123@naver.com>
2023-05-17 23:17:43 +0900

    230517 coding test 연습

--- Programmers_Coding_Test/src/level0/MorseCode.java
+++ Programmers_Coding_Test/src/level0/MorseCode.java
@@ -1,9 +1,47 @@
 package level0;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MorseCode {
 
 	public String solution(String letter) {
-        String answer = "";
+		Map<String, String> decode = new HashMap<>();
+		decode.put(".-", "a");
+		decode.put("-...", "b");
+		decode.put("-.-.", "c");
+		decode.put("-..", "d");
+		decode.put(".", "e");
+		decode.put("..-.", "f");
+		decode.put("--.", "g");
+		decode.put("....", "h");
+		decode.put("..", "i");
+		decode.put(".---", "j");
+		decode.put("-.-", "k");
+		decode.put(".-..", "l");
+		decode.put("--", "m");
+		decode.put("-.", "n");
+		decode.put("---", "o");
+		decode.put(".--.", "p");
+		decode.put("--.-", "q");
+		decode.put(".-.", "r");
+		decode.put("...", "s");
+		decode.put("-", "t");
+		decode.put("..-", "u");
+		decode.put("...-", "v");
+		decode.put(".--", "w");
+		decode.put("-..-", "x");
+		decode.put("-.--", "y");
+		decode.put("--..", "z");
+		
+		String[] array = letter.split(" ");
+		
+		String answer = "";
+		
+		for (int i = 0; i < array.length; i++) {
+			answer += decode.get(array[i]);
+		}
+        
         return answer;
     }
 }
