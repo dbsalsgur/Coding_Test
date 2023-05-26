baae8e823f269be099c816f590bd1f1b5c2dc4cc
dbsalsgur <rkawkaos123@naver.com>
2023-05-27 02:38:54 +0900

    230527 coding test 연

---
+++ Programmers_Coding_Test/src/level0/BallThrowing.java
@@ -0,0 +1,12 @@
+package level0;
+
+public class BallThrowing {
+
+	public int solution(int[] numbers, int k) {
+		while(true) {
+			numbers 
+		}
+        int answer = 0;
+        return answer;
+    }
+}
