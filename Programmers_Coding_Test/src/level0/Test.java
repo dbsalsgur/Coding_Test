5f7f123edd19d65140d9961628ad36018e709538
dbsalsgur <rkawkaos123@naver.com>
2023-06-08 00:21:03 +0900

    230608 coding test 연습

--- Programmers_Coding_Test/src/level0/Test.java
+++ Programmers_Coding_Test/src/level0/Test.java
@@ -3,24 +3,18 @@ package level0;
 public class Test {
 
 	public static void main(String[] args) {
-		int n = 15;
+		int n = 7;
 		
 		int answer = 0;
-        
-        for (int i = 1; i <= 10; i++) {
-        	int cnt = 0;
-        	for (int j = 1; j <= n; j++) {
-    			if (i%j == 0) {
-    				cnt++;
-    			}
-    			System.out.println("cnt in for = "+cnt);
-    		}
-        	if (cnt >= 3) {
-				answer++;
-				System.out.println("cnt = "+cnt);
+		int temp = 1;
+		for (int i = 1; true; i++) {
+			temp *= i;
+			if (temp >= n) {
+				break;
 			}
-        	cnt = 0;
-        	System.out.println(i);
+			System.out.println("temp = "+temp);
+			answer++;
+			System.out.println(answer);
 		}
         System.out.println(answer);
 	}
