76f5617cf11490149f3acf6f425a4ada45f83f08
dbsalsgur <rkawkaos123@naver.com>
2023-06-11 23:57:14 +0900

    230611 coding test 연습

--- Programmers_Coding_Test/src/level0/Test.java
+++ Programmers_Coding_Test/src/level0/Test.java
@@ -1,16 +1,29 @@
 package level0;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Test {
 
 	public static void main(String[] args) {
-		String my_string = "nice to meet you";
+		String my_string = "hi12392";
+		Pattern pattern = Pattern.compile("^[0-9]*");
+		String matched_num = "";
 		
-		String answer = my_string;
-		String[] vowels = {"a", "e", "i", "o", "u"};
-		for (String string : vowels) {
-			answer = answer.replaceAll(string, "");
+		Matcher matcher = pattern.matcher(my_string);
+		
+		while (matcher.find()) {
+			matched_num += matcher.group();
+			System.out.println(matcher.group());
 		}
-        System.out.println(answer);
+		System.out.println(matched_num);
+        int[] answer = new int[matched_num.length()];
+        for (int i = 0; i < answer.length; i++) {
+			answer[i] = matched_num.charAt(i) - '0';
+		}
+        for (int i : answer) {
+			System.out.println(i);
+		}
 	}
 }
 
