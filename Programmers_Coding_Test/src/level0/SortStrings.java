469bdcdb5299c3f35c9e9615b1207447f56999d3
dbsalsgur <rkawkaos123@naver.com>
2023-06-12 00:08:10 +0900

    230612 coding test 연습

--- Programmers_Coding_Test/src/level0/SortStrings.java
+++ Programmers_Coding_Test/src/level0/SortStrings.java
@@ -1,14 +1,24 @@
 package level0;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SortStrings {
 	public int[] solution(String my_string) {
-		Pattern pattern = Pattern.compile("^[0-9]*");
+		Pattern pattern = Pattern.compile("[0-9]");
+		String matched_num = "";
 		
-		String matched_string = pattern.pattern();
-        int[] answer = {};
+		Matcher matcher = pattern.matcher(my_string);
+		
+		while (matcher.find()) {
+			matched_num = matched_num + matcher.group();
+		}
+        int[] answer = new int[matched_num.length()];
+        for (int i = 0; i < answer.length; i++) {
+			answer[i] = matched_num.charAt(i) - '0';
+		}
+        Arrays.sort(answer);
         return answer;
     }
 }
--- Programmers_Coding_Test/src/level0/Test.java
+++ Programmers_Coding_Test/src/level0/Test.java
@@ -1,5 +1,6 @@
 package level0;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -7,20 +8,21 @@ public class Test {
 
 	public static void main(String[] args) {
 		String my_string = "hi12392";
-		Pattern pattern = Pattern.compile("^[0-9]*");
+		
+		Pattern pattern = Pattern.compile("[0-9]");
 		String matched_num = "";
 		
 		Matcher matcher = pattern.matcher(my_string);
 		
 		while (matcher.find()) {
-			matched_num += matcher.group();
+			matched_num = matched_num + matcher.group();
 			System.out.println(matcher.group());
 		}
-		System.out.println(matched_num);
         int[] answer = new int[matched_num.length()];
         for (int i = 0; i < answer.length; i++) {
 			answer[i] = matched_num.charAt(i) - '0';
 		}
+        Arrays.sort(answer);
         for (int i : answer) {
 			System.out.println(i);
 		}
