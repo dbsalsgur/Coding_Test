41498782c607d9e1c2209f8edd0dda34daa5540a
dbsalsgur <rkawkaos123@naver.com>
2023-07-01 19:08:34 +0900

    230701 coding test 연습

--- Programmers_Coding_Test/src/level0/AreaOfRectangle.java
+++ Programmers_Coding_Test/src/level0/AreaOfRectangle.java
@@ -3,10 +3,19 @@ package level0;
 public class AreaOfRectangle {
 	public int solution(int[][] dots) {
 		
-		int answer = (dots[1][0]-dots[0][0])*(dots[2][1]-dots[1][1]);
-		if (answer < 0) {
-			answer *= -1;
+		int answer = 0;
+		int minX = dots[0][0];
+		int minY = dots[0][1];
+		int maxX = dots[0][0];
+		int maxY = dots[0][1];
+		
+		for (int i = 0; i < dots.length; i++) {
+			minX = Math.min(minX, dots[i][0]);
+			minY = Math.min(minY, dots[i][1]);
+			maxX = Math.max(maxX, dots[i][0]);
+			maxY = Math.max(maxY, dots[i][1]);
 		}
+		answer = (maxX - minX)*(maxY - minY);
         return answer;
     }
 }
