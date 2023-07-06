0434f50d76644056865846639bad0adfa6318fa9
dbsalsgur <rkawkaos123@naver.com>
2023-07-06 21:22:50 +0900

    230706 coding test연습

--- Programmers_Coding_Test/src/level0/CharacterCoordinates.java
+++ Programmers_Coding_Test/src/level0/CharacterCoordinates.java
@@ -2,7 +2,20 @@ package level0;
 
 public class CharacterCoordinates {
 	public int[] solution(String[] keyinput, int[] board) {
-        int[] answer = {};
+        int[] answer = {0,0};
+        int maxX = board[0]/2;
+        int maxY = board[1]/2;
+        for (int i = 0; i < keyinput.length; i++) {
+			if (keyinput[i].equals("up")) {
+				answer[1] += 1;
+			} else if(keyinput[i].equals("down")){
+				answer[1] -= 1;
+			} else if(keyinput[i].equals("left")){
+				answer[0] -= 1;
+			} else if(keyinput[i].equals("right")){
+				answer[0] += 1;
+			}
+		}
         return answer;
     }
 }
