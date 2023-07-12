47f70a118948a6fe6491b1c1e2c5910aee88b6a5
dbsalsgur <rkawkaos123@naver.com>
2023-07-12 09:55:19 +0900

    230712 coding test 연습

--- Programmers_Coding_Test/src/level0/Test.java
+++ Programmers_Coding_Test/src/level0/Test.java
@@ -1,43 +1,20 @@
 package level0;
 
+import java.util.Arrays;
 
 public class Test {
 
 	public static void main(String[] args) {
-		String[] keyinput = {"left", "right", "up", "right", "right"};
-		int[] board = {1, 11};
+		int[] numbers = {1,2,-3,4,-5};
 		
-		int[] answer = {0,0};
-        int maxX = (board[0]-1)/2;
-        int maxY = (board[1]-1)/2;
-        for (int i = 0; i < keyinput.length; i++) {
-			if (keyinput[i].equals("up")) {
-				answer[1]++;
-			} else if(keyinput[i].equals("down")){
-				answer[1]--;
-			} else if(keyinput[i].equals("left")){
-				answer[0]--;
-			} else if(keyinput[i].equals("right")){
-				answer[0]++;
-			}
-		}
-        
-        if (answer[1] > maxY) {
-			 answer[1] = maxY;
-		}
-        if(answer[1] < -maxY) {
-			answer[1] = -maxY;
-		}
-        if(answer[0] < -maxX) {
-			answer[0] = -maxX;
-		}
-        if (answer[0] > maxX) {
-			answer[0] = maxX;
-		}
-        
-        for (int i : answer) {
-			System.out.println(i);
-		}
+		Arrays.sort(numbers);
+		int value1 = 0;
+		int value2 = 0;
+		value1 = numbers[0]*numbers[1];
+		value2 = numbers[numbers.length-1]*numbers[numbers.length-2];
+		
+        int answer = value1 > value2 ? value1 : value2;
+        System.out.println(answer);
 	}
 }
 
