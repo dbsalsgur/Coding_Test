32bac42d8d4984d6974d2e495f8c23f494870af3
dbsalsgur <rkawkaos123@naver.com>
2023-07-26 23:38:00 +0900

    230726 coding test 연습

--- Programmers_Coding_Test/src/level0/CompletionConditionOfTriangle.java
+++ Programmers_Coding_Test/src/level0/CompletionConditionOfTriangle.java
@@ -3,6 +3,16 @@ package level0;
 public class CompletionConditionOfTriangle {
 	public int solution(int[] sides) {
         int answer = 0;
+        int a = Math.max(sides[0], sides[1]);
+        int b = Math.min(sides[0], sides[1]);
+        for (int i = 0; i < a-b; i++) {
+			answer++;
+		}
+        for (int i = 0; i < a+b; i++) {
+			if (i > a) {
+				answer++;
+			}
+		}
         return answer;
     }
 }
--- Programmers_Coding_Test/src/level0/Test.java
+++ Programmers_Coding_Test/src/level0/Test.java
@@ -3,32 +3,19 @@ package level0;
 public class Test {
 
 	public static void main(String[] args) {
-		int[][] board =  {{1,1,1,1,1},{1,1,1,1,1},{1,1,1,1,1},{1,1,1,1,1},{1,1,1,1,1}};
-		int[][] mineZone = new int[board.length+2][board.length+2];
+		int[] sides = {11, 7};
 		
 		int answer = 0;
-		int count = 0;
-		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board.length; j++) {
-				if (board[i][j] == 0) {
-					continue;
-				} else {
-					for (int k = (i-1)+1; k <= (i+1)+1 ; k++) {
-						for (int l = (j-1)+1; l <= (j+1)+1; l++) {
-							mineZone[k][l] = 1;
-						}
-					}
-				}
+        int a = Math.max(sides[0], sides[1]);
+        int b = Math.min(sides[0], sides[1]);
+        for (int i = 0; i <= a-b; i++) {
+			answer++;
+		}
+        for (int i = 0; i <= a+b; i++) {
+			if (i >= a) {
+				answer++;
 			}
 		}
-        for (int i = 1; i < mineZone.length-1; i++) {
-			for (int j = 1; j < mineZone.length-1; j++) {
-				if (mineZone[i][j] == 1) {
-					count++;
-				}
-			}
-		}
-		answer = board.length * board.length - count;
         System.out.println(answer);
 	}
 }
